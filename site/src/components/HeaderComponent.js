b0a6dec161aee6a8d50b5983d41e69b69e867245
Lawrence McDaniel <lpm0073@gmail.com>
2020-05-13 18:14:08 -0500

    add remote urls to navbar

--- site/src/components/HeaderComponent.js
+++ site/src/components/HeaderComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Navbar, Nav, NavbarToggler, Collapse, NavItem} from 'reactstrap';
+import { Navbar, Nav, NavbarToggler, Collapse, NavItem,
+    UncontrolledDropdown, DropdownMenu, DropdownToggle, DropdownItem} from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
@@ -24,11 +25,11 @@ class Header extends Component {
     render() {
         return(
             <React.Fragment>
-                <Navbar dark expand="md" fixed="top">
-                    <div className="container">
-                        <NavbarToggler onClick={this.toggleNav} />
-                        <Collapse isOpen={this.state.isNavOpen} navbar>
-                            <Nav navbar>
+                <div className="container">
+                <Navbar className="navbar-dark bg-dark" dark expand="md" fixed="top">
+                    <NavbarToggler onClick={this.toggleNav} />
+                    <Collapse isOpen={this.state.isNavOpen} navbar>
+                        <Nav navbar>
                             <NavItem>
                                 <NavLink className="nav-link"  to='/'>Home</NavLink>
                             </NavItem>
@@ -54,17 +55,45 @@ class Header extends Component {
                             <NavItem>
                                 <NavLink className="nav-link" to='/contact'>Contact</NavLink>
                             </NavItem>
-                            <NavItem>
-                                <NavLink className="nav-link"  to='/code'>Code</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink className="nav-link"  to='/blog'>Blog</NavLink>
-                            </NavItem>
 
-                            </Nav>
-                        </Collapse>
-                    </div>
+                            <UncontrolledDropdown nav inNavbar>
+                                <DropdownToggle nav caret>Code</DropdownToggle>
+                                <DropdownMenu >
+                                    <DropdownItem >
+                                        <a className="mx-1" href='https://www.sustainableimpactfund.com/' target="_blank" rel="noopener noreferrer">Wordpress Site</a>
+                                    </DropdownItem>
+                                    <DropdownItem>
+                                        <a className="mx-1" href='https://clients.lawrencemcdaniel.com/' target="_blank" rel="noopener noreferrer">Ecommerce Site</a>
+                                    </DropdownItem>
+                                    <DropdownItem>
+                                        <a className="mx-1" href='https://my.roverbyopenstax.org/' target="_blank" rel="noopener noreferrer">Open edX Site</a>
+                                    </DropdownItem>
+                                    <DropdownItem>
+                                        <a className="mx-1" href='https://horrors.lawrencemcdaniel.com/' target="_blank" rel="noopener noreferrer">AngularJS Site</a>
+                                    </DropdownItem>
+                                    <DropdownItem>
+                                        <a className="mx-1" href='/' rel="noopener noreferrer">ReactJS (This Site)</a>
+                                    </DropdownItem>
+                                    <DropdownItem>
+                                        <a className="mx-1" href='https://edx.lawrencemcdaniel.com/' target="_blank" rel="noopener noreferrer">Pure HTML</a>
+                                    </DropdownItem>
+                                    <DropdownItem>
+                                        <a className="mx-1" href='https://webdev-class.lawrencemcdaniel.com/' target="_blank" rel="noopener noreferrer">Bootstrap</a>
+                                    </DropdownItem>
+                                    <DropdownItem divider />
+                                    <DropdownItem>
+                                        <a className="mx-1" href='https://github.com/lpm0073' target="_blank" rel="noopener noreferrer">Me on Github</a>
+                                    </DropdownItem>
+                                </DropdownMenu>
+                            </UncontrolledDropdown>
+
+                            <NavItem>
+                                <NavLink className="nav-link" to='https://blog.lawrencemcdaniel.com/'>Blog</NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
                 </Navbar>
+                </div>
 
             </React.Fragment>
         );
