043fb84327ec36f4b3733d8c90f00f0c9951c588
Lawrence McDaniel <lpm0073@gmail.com>
2020-05-13 15:16:33 -0500

    add code sample cta

--- site/src/components/SpecialtiesPage.js
+++ site/src/components/SpecialtiesPage.js
@@ -5,19 +5,30 @@ import SpecialtiesCarousel from '../widgets/specialtiesCarousel';
 
 const Specialties = (props) => {
 
+    return(
+        <React.Fragment>
+            <div className="specialties-page">
+                <div className="container site-page ">
+                    <RenderPageTitle icon="fa-pencil-square-o" title="MY" boxed_title="SPECIALTIES" />
+                    <SpecialtiesBanner />
+                    <SpecialtiesCarousel />
 
-        return(
-            <React.Fragment>
-              <div className="specialties-page">
-                  <div className="container site-page ">
-                      <RenderPageTitle icon="fa-pencil-square-o" title="MY" boxed_title="SPECIALTIES" />
-                      <SpecialtiesBanner />
-                      <SpecialtiesCarousel />
-                  </div>
-              </div>              
+                    <div className="row specialties-code-samples mt-5 p-3">
+                        <div className="col">
+                            <div>
+                                <h2>Want to see code samples?</h2>
+                                <h6>I'm constantly experimenting with new GitHub libraries and coding techniques. Click here to see some of my all-time favorites.</h6>
+                            </div>
+                        </div>
+                        <div className="col-2">
+                        <a className="btn btn-danger" role="button" href="http://horrors.lawrencemcdaniel.com/" title="AngularJS Code Samples" target="_blank" rel="noopener noreferrer"><i className="fa fa-cogs"></i> Code samples</a>
+                        </div>
+                    </div>
+                </div>
+            </div>              
 
-            </React.Fragment>
-        );
+        </React.Fragment>
+    );
 
 }
 
