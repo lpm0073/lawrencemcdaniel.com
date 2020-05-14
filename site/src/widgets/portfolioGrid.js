384ca3f8357405efcbac9af02ebbd0a376891d19
Lawrence McDaniel <lpm0073@gmail.com>
2020-05-14 10:50:14 -0500

    style portfolio grid

--- site/src/widgets/portfolioGrid.js
+++ site/src/widgets/portfolioGrid.js
@@ -31,16 +31,16 @@ class PortfolioGrid extends Component {
           ) : (
             <div id="portfolio-grid">
               <div className="row m-5 text-center">
-                {this.state.data.map((specialty, indx) => {
-                  const item_url = specialty._embedded['wp:featuredmedia'][0].source_url;
+                {this.state.data.map((portfolio_item, indx) => {
+                  const item_url = portfolio_item._embedded['wp:featuredmedia'][0].source_url;
                   const background_url = "url('" + item_url + "')";
                   const item_style = {
                     "background-image": background_url
                   }
                   return (
-                    <div className="col-md-3 portfolio-item" key={indx}>
+                    <div className="col-md-3 portfolio-item p-1" key={indx}>
                         <div 
-                            className="portfolio-item-internal" 
+                            className="portfolio-item-internal m-0" 
                             style={item_style}>
                         </div>
                     </div>
