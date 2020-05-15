acacf3786c4baf8ec1bb10cba1365feff4591872
Lawrence McDaniel <lpm0073@gmail.com>
2020-05-15 14:12:32 -0500

    skills page styling

--- site/src/pages/skills/skillColumn.js
+++ site/src/pages/skills/skillColumn.js
@@ -3,16 +3,16 @@ import React from 'react';
 
 const SkillColumn = (props) => {
     
-    const thisClassName = "fa " + props.icon + " fa-2x";
+    const thisClassName = "fa p-3 " + props.icon + " fa-2x";
 
     return(
         <React.Fragment>
-            <div className="col-lg-2 col-md-4 col-sm-6">
+            <div className="col-lg-2 col-md-4 col-sm-6 px-1">
                 <div className="skill-column text-center">
                     <div className="skill-pct">{props.pct}%</div>
-                    <h4 className="skill-title my-4">{props.title}</h4>
-                    <p className="skill-description">{props.description}</p>
-                    <div className="skill-icon mt-5">
+                    <h4 className="skill-title mt-4">{props.title}</h4>
+                    <p className="skill-description px-2">{props.description}</p>
+                    <div className="skill-icon">
                         <i className={thisClassName}></i>
                     </div>
                 </div>
