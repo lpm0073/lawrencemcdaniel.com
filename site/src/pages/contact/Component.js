67a9034ad121491108513bc028c509cc7318f7e4
Lawrence McDaniel <lpm0073@gmail.com>
2020-05-16 18:41:44 -0500

    contact page styling

--- site/src/pages/contact/Component.js
+++ site/src/pages/contact/Component.js
@@ -13,21 +13,25 @@ class Contact extends Component {
                 <div className="site-page ">
                     <RenderPageTitle theme="light" icon="fa-phone" title="LETS" boxed_title="TALK" />
     
-                    <div className="row text-center">
-                        <div className="col">
-                            <address>
-                            210 Broadway, Suite 200<br />
-                            Cambridge, MA 02139<br />
-                            USA
-                            </address>
-                            <a href="tel:+1 (617) 834-6172">+1 (617) 834-6172</a>                                
+                    <div className="row text-center mt-5">
+                        <div className="col contact-data">
+                            <a target="_blank" href="https://goo.gl/maps/WJy25bNGit2L8Nzz8" rel="noopener noreferrer">
+                                <i class="fa fa-map-marker fa-3x"></i>
+                                <address className="m-b-4">
+                                Lawrence McDaniel, Full Stack Developer<br />
+                                210 Broadway, Suite 200<br />
+                                Cambridge, MA 02139<br />
+                                USA
+                                </address>
+                            </a>
+                            <a className="m-4 contact-phone-link" href="tel:+1 (617) 834-6172">+1 (617) 834-6172</a>                                
 
-                            <div className="contact-social-buttons">
-                                <a target="_blank" href="https://mx.linkedin.com/in/lawrencemcdaniel"><i class="fa fa-linkedin-square fa-3x"></i></a>
-                                <a target="_blank" href="https://www.facebook.com/lawrence.p.mcdaniel"><i class="fa fa-facebook-square fa-3x"></i></a>
-                                <a target="_blank" href="https://plus.google.com/u/0/+LawrenceMcDanielEstler"><i class="fa fa-github-square fa-3x"></i></a>
+                            <div className="contact-social-buttons m-4">
+                                <a className="m-1" target="_blank" href="https://mx.linkedin.com/in/lawrencemcdaniel" rel="noopener noreferrer"><i class="fa fa-linkedin-square fa-3x"></i></a>
+                                <a className="m-1" target="_blank" href="https://www.facebook.com/lawrence.p.mcdaniel" rel="noopener noreferrer"><i class="fa fa-facebook-square fa-3x"></i></a>
+                                <a className="m-1" target="_blank" href="https://github.com/lpm0073" rel="noopener noreferrer"><i class="fa fa-github-square fa-3x"></i></a>
                             </div>
-                            <h4>Drop me a Line at <a href="mailto:lpm0073@gmail.com">lpm0073@gmail.com</a> or Skype me at mcdaniel0073</h4>
+                            <h4>Drop me a line at <a href="mailto:lpm0073@gmail.com">lpm0073@gmail.com</a> or Skype me at mcdaniel0073</h4>
 
                         </div>
                     </div>
