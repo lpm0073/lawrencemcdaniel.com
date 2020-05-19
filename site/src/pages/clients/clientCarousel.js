a8ceb2381462bb44420c172e1d5596daae889d86
Lawrence McDaniel <lpm0073@gmail.com>
2020-05-19 11:39:00 -0500

    client carousel styling

--- site/src/pages/clients/clientCarousel.js
+++ site/src/pages/clients/clientCarousel.js
@@ -5,16 +5,16 @@ import Loading from '../../components/Loading';
 
 const responsive = {
   superLargeDesktop: {
-    breakpoint: { max: 4000, min: 3000 },
+    breakpoint: { max: 10000, min: 1500 },
     items: 4
   },
   desktop: {
-    breakpoint: { max: 3000, min: 1024 },
-    items: 4
+    breakpoint: { max: 1500, min: 1024 },
+    items: 3
   },
   tablet: {
     breakpoint: { max: 1024, min: 464 },
-    items: 3
+    items: 2
   },
   mobile: {
     breakpoint: { max: 464, min: 0 },
