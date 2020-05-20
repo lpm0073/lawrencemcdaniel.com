e7fab55858e06328a75a66fc554817dc3189dae8
Lawrence McDaniel <lpm0073@gmail.com>
2020-05-19 19:06:00 -0500

    prep for production

--- site/src/pages/skills/Component.js
+++ site/src/pages/skills/Component.js
@@ -15,17 +15,17 @@ const Skills = (props) => {
                     <RenderPageTitle theme="light" icon="fa-book" title="HARD" boxed_title="SKILLS" />
 
                     <div className="row mt-5 mb-5">
-                        <SkillColumn pct="100" title="Web" icon="fa-code" description="Complete professional web sites and web apps using WordPress, Django, Angular, and Ionic" />
+                        <SkillColumn pct="100" title="Web" icon="fa-code" description="Complete professional web sites and web apps using Django, ReactJS, Redux, Angular, and WordPress" />
                         <SkillColumn pct="100" title="Mobile & IoT" icon="fa-mobile" description="Turnkey hybrid mobile apps with Ionic-Cordova. Complete IoT designs with Android-DragonBoard" />
-                        <SkillColumn pct="100" title="Cloud" icon="fa-cloud" description="Efficient, highly-available and horizontally-scalable infrastructure designs for LAMP and .Net on AWS" />
+                        <SkillColumn pct="100" title="Cloud" icon="fa-cloud" description="Server migrations, and consultation on cloud infrastructure planning to minimize costs while maximizing security and performance." />
                         <SkillColumn pct="100" title="Data" icon="fa-database" description="OLTP and BI designs & programming for MySQL and MS-SQL Server. High-availability designs for MongoDB, and MapReduce problems on Hadoop" />
-                        <SkillColumn pct="100" title="DevOps" icon="fa-server" description="Create and maintain continuous integration strategies using GitHub, Capistrano, Jenkins, Chef, and Puppet" />
+                        <SkillColumn pct="100" title="DevOps" icon="fa-server" description="Create and maintain continuous integration strategies using GitHub, AWS Boto3, Jenkins, Chef, Puppet or Ansible." />
                         <SkillColumn pct="100" title="Machine Learning" icon="fa-magic" description="Applied Linear Algebra, AI and Neural Networks designs and coding using Octave and Python scikit-learn and NumPy" />
                     </div>
 
                     <div className="row mt-5 mb-5">
                         <div className="col-lg-6 col-md-12">
-                            <SkillBar pct="100" description="Django, React, Angular, Ionic, WordPress" />
+                            <SkillBar pct="100" description="Django, React, Redux, Angular, Ionic, WordPress" />
                             <SkillBar pct="100" description="Bootstrap, D3, Underscore.js" />
                             <SkillBar pct="100" description="MySQL, MongoDB, Hadoop, MS-SQL, Transact-SQL " />
                             <SkillBar pct="90" description="AWS, Boto3, Linux, Bash, Ansible, Jenkins, Chef, Puppet" />
