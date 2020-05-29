d763d962286b6bf859cc336c01cb329419da7cbc
Lawrence McDaniel <lpm0073@gmail.com>
2020-05-29 08:40:34 -0500

    fix body/container margins and centering

--- site/src/components/Main.js
+++ site/src/components/Main.js
@@ -10,7 +10,7 @@ class Main extends Component {
 
   render() {
     return ( 
-      <div className="container">
+      <div className="container-fluid p-0">
         <Header />
         <Routes />
         <Footer />
