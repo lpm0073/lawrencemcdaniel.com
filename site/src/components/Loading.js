1c443aedb77fd9ef7f6ce9bda07e2244bd9dfca5
Lawrence McDaniel <lpm0073@gmail.com>
2020-06-01 16:10:29 -0500

    switch to react-loader-spinner

--- site/src/components/Loading.js
+++ site/src/components/Loading.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import Loader from 'react-loader-spinner'
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
 
 const Loading = () => {
     return(
         <div className="col-12 text-center">
-            <span className="fa fa-spinner fa-pulse fa-3x fa-fw"></span>
+            <Loader
+                type="ThreeDots"
+                color="#038fc5"
+                height={100}
+                width={100}
+                timeout={3000}
+            />
         </div>
     );
 }
