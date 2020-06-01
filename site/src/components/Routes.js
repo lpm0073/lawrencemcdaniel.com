60560196a7c01374f5cb38cba9087f1f5a762b95
Lawrence McDaniel <lpm0073@gmail.com>
2020-06-01 10:49:26 -0500

    lazy load all pages

--- site/src/components/Routes.js
+++ site/src/components/Routes.js
@@ -8,19 +8,22 @@ import { fetchSpecialties, fetchPortfolio, fetchEducation,
           fetchRecommendations, fetchProjectImages,
           fetchClients } from '../redux/ActionCreators';
 
+
 // Pages
-import Home from '../pages/home/Component';
-import Contact from '../pages/contact/Component';
-import About from '../pages/about/Component';
-import Bio from '../pages/bio/Component';
-import Specialties from '../pages/specialties/Component';
-import Portfolio from '../pages/portfolio/Component';
-import PortfolioDetail from '../pages/portfolioDetail/Component';
-import Education from '../pages/education/Component';
-import Skills from '../pages/skills/Component';
-import Clients from '../pages/clients/Component';
-import MachineLearning from '../pages/machineLearning/Component';
-import ReactPage from '../pages/react/Component';
+import { Suspense } from 'react';
+import Loading from './Loading';
+const Home = React.lazy(() => import('../pages/home/Component'));
+const Contact = React.lazy(() => import('../pages/contact/Component'));
+const About = React.lazy(() => import('../pages/about/Component'));
+const Bio = React.lazy(() => import('../pages/bio/Component'));
+const Specialties = React.lazy(() => import('../pages/specialties/Component'));
+const Portfolio = React.lazy(() => import('../pages/portfolio/Component'));
+const PortfolioDetail = React.lazy(() => import('../pages/portfolioDetail/Component'));
+const Education = React.lazy(() => import('../pages/education/Component'));
+const Skills = React.lazy(() => import('../pages/skills/Component'));
+const Clients = React.lazy(() => import('../pages/clients/Component'));
+const MachineLearning = React.lazy(() => import('../pages/machineLearning/Component'));
+const ReactPage = React.lazy(() => import('../pages/react/Component'));
 
 
 const mapStateToProps = state => {
@@ -58,37 +61,130 @@ class Routes extends Component {
 
   render() {
 
+    const HomePage = () => {
+
+      return(
+        <Suspense fallback={Loading}>
+          <Home logos={this.props.logos} />
+        </Suspense>
+      );
+    }
+    const ContactPage = () => {
+
+      return(
+        <Suspense fallback={Loading}>
+          <Contact />
+        </Suspense>
+      );
+    }
+    const AboutPage = () => {
+
+      return(
+        <Suspense fallback={Loading}>
+          <About />
+        </Suspense>
+      );
+    }
+
+    const MachineLearningPage = () => {
+
+      return(
+        <Suspense fallback={Loading}>
+          <MachineLearning  />
+        </Suspense>
+      );
+    }
+    const BioPage = () => {
+
+      return(
+        <Suspense fallback={Loading}>
+          <Bio />
+        </Suspense>
+      );
+    }
+    const SpecialtiesPage = () => {
+
+      return(
+        <Suspense fallback={Loading}>
+          <Specialties specialties={this.props.specialties} />
+        </Suspense>
+      );
+    }
+    const PortfolioPage = () => {
+
+      return(
+        <Suspense fallback={Loading}>
+          <Portfolio portfolio={this.props.portfolio} />}
+        </Suspense>
+      );
+    }
     const PortfolioWithSlug = ({match}) => {
 
       return(
-        <PortfolioDetail post={this.props.portfolio.projects.filter((item) => item.slug === match.params.portfolioId)[0]} 
-            postLoading={this.props.portfolio.isLoading}
-            postErrMess={this.props.portfolio.errMess}
-            images={this.props.project}
-            imagesLoading={this.props.project.isLoading}
-            imagesError={this.props.project.errMess}
-        />
+        <Suspense fallback={Loading}>
+          <PortfolioDetail post={this.props.portfolio.projects.filter((item) => item.slug === match.params.portfolioId)[0]} 
+              postLoading={this.props.portfolio.isLoading}
+              postErrMess={this.props.portfolio.errMess}
+              images={this.props.project}
+              imagesLoading={this.props.project.isLoading}
+              imagesError={this.props.project.errMess}
+          />
+        </Suspense>
+      );
+    }
+
+
+    const EducationPage = () => {
+
+      return(
+        <Suspense fallback={Loading}>
+          <Education education={this.props.education} />
+        </Suspense>
+      );
+    }
+    const SkillsPage = () => {
+
+      return(
+        <Suspense fallback={Loading}>
+          <Skills />
+        </Suspense>
+      );
+    }
+    const ReactPagePage = () => {
+
+      return(
+        <Suspense fallback={Loading}>
+          <ReactPage />
+        </Suspense>
+      );
+    }
+    const ClientsPage = () => {
+
+      return(
+        <Suspense fallback={Loading}>
+          <Clients 
+            recommendations={this.props.recommendations}
+            clients={this.props.clients}
+            />} /> 
+        </Suspense>
       );
     }
 
     return(
         <React.Fragment>
           <Switch>
-            <Route path="/home" component={() => <Home logos={this.props.logos} />} />
-            <Route exact path="/contact" component={Contact} />
-            <Route exact path="/about" component={About} />
-            <Route exact path="/machine-learning" component={MachineLearning} />
-            <Route exact path="/bio" component={Bio} />
-            <Route exact path="/specialties" component={() => <Specialties specialties={this.props.specialties} />} />
-            <Route exact path="/portfolio" component={() => <Portfolio portfolio={this.props.portfolio} />} />
+            <Route path="/home" component={HomePage} />
+            <Route exact path="/contact" component={ContactPage} />
+            <Route exact path="/about" component={AboutPage} />
+            <Route exact path="/machine-learning" component={MachineLearningPage} />
+            <Route exact path="/bio" component={BioPage} />
+            <Route exact path="/specialties" component={SpecialtiesPage} />
+            <Route exact path="/portfolio" component={PortfolioPage} />
             <Route path="/portfolio/:portfolioId" component={PortfolioWithSlug} />
-            <Route exact path="/education" component={() => <Education education={this.props.education} />} />
-            <Route exact path="/skills" component={Skills} />
-            <Route exact path="/reactjs" component={ReactPage} />
-            <Route exact path="/clients" component={() => <Clients 
-                                                              recommendations={this.props.recommendations}
-                                                              clients={this.props.clients}
-                                                             />} />
+            <Route exact path="/education" component={EducationPage} />
+            <Route exact path="/skills" component={SkillsPage} />
+            <Route exact path="/reactjs" component={ReactPagePage} />
+            <Route exact path="/clients" component={ClientsPage} />
             <Redirect to="/home" />
           </Switch>
         </React.Fragment>
