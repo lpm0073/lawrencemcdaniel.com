6e7d43b671142e9cb535a3ab2b8e05b0312845ec
Lawrence McDaniel <lpm0073@gmail.com>
2020-06-01 10:25:57 -0500

    set cache-control headers

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,23 @@
 #
 # usage:      deploy prouduction build of lawrencemcdaniel.com ReactJ
 #             app to AWS S3 bucket.
+#
+#             https://gist.github.com/kellyrmilligan/e242d3dc743105fe91a83cc933ee1314
 #---------------------------------------------------------
-aws s3 sync ./site/build/ s3://reactjs.lawrencemcdaniel.com --acl public-read
-aws s3 cp ./site/build/index.html s3://reactjs.lawrencemcdaniel.com --acl public-read --cache-control "no cache"
+
+# ------------------------
+# add all built files to the S3 bucket.
+# ------------------------
+aws s3 sync ./site/build/ s3://reactjs.lawrencemcdaniel.com --acl public-read --delete --cache-control max-age=31536000,public
+
+# ------------------------
+# remove the cache-control header created above with a "no-cache" header so that browsers never cache this page
+# ------------------------
+aws s3 cp s3://reactjs.lawrencemcdaniel.com/service-worker.js s3://reactjs.lawrencemcdaniel.com/service-worker.js --metadata-directive REPLACE --cache-control max-age=0,no-cache,no-store,must-revalidate --content-type application/javascript --acl public-read
+
+aws s3 cp s3://reactjs.lawrencemcdaniel.com/index.html s3://reactjs.lawrencemcdaniel.com/index.html --metadata-directive REPLACE --cache-control max-age=0,no-cache,no-store,must-revalidate --content-type text/html --acl public-read
+
+# invalidate the Cloudfront cache
+aws cloudfront create-invalidation \
+              --distribution-id E2364TSMHRWAWL \
+              --paths "/*" "/index.html"
