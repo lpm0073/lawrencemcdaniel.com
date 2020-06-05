3c13135cb16b9f214177003a25e9cdebd8b11d99
Lawrence McDaniel <lpm0073@gmail.com>
2020-06-05 17:51:08 -0500

    update resume URL

--- site/src/shared/urls.js
+++ site/src/shared/urls.js
@@ -1,3 +1,3 @@
 export const baseUrl = 'http://localhost:3001/';
-export const resumeUrl = 'https://cdn.lawrencemcdaniel.com/doc/lawrence-mcdaniel-resume-201911.pdf';
+export const resumeUrl = 'https://cdn.lawrencemcdaniel.com/doc/Resume-Lawrence-McDaniel-202006.pdf';
 export const backendUrl = 'https://api.lawrencemcdaniel.com/wp-json/wp/v2/';
