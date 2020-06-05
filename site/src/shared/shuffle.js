669288e2bf33668cba0ddfc6e2e2d025f9fdd2b5
Lawrence McDaniel <lpm0073@gmail.com>
2020-06-05 11:06:26 -0500

    minor optimizations

--- site/src/shared/shuffle.js
+++ site/src/shared/shuffle.js
@@ -1,24 +1,11 @@
-/*
-export const shuffleArray = array => {
-    let i = array.length - 1;
-    for (; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      const temp = array[i];
-      array[i] = array[j];
-      array[j] = temp;
-    }
-    return array;
-  }
-*/
-
-
 export const shuffleArray = array => {
 
+  const n = array.length;
   var tmp = [];
   var j = 0;
   for (var i = 0; i < array.length; i++) {
       while (tmp.includes(array[j])) {
-        j = Math.floor(Math.random() * array.length);
+        j = Math.floor(Math.random() * n);
       }
       tmp.push(array[j]);
     }
