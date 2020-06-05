ed67e3e0767ff7b3791c91ef3535fa384da290d2
Lawrence McDaniel <lpm0073@gmail.com>
2020-06-05 12:13:50 -0500

    linter

--- site/src/components/logocube/Component.js
+++ site/src/components/logocube/Component.js
@@ -167,55 +167,6 @@ class LogoCube extends Component {
         this.fetcher = this.fetcher.bind(this);
         }
 
-    fetcher() {
-        const self = this;
-        fetch(backendUrl + "posts?categories=43&_embed&per_page=100")
-        .then(
-            response => {
-                if (response.ok) {
-                    return response;
-                } else {
-                    var error = new Error('Error ' + response.status + ': ' + response.statusText);
-                    error.response = response;
-                    throw error;
-                }
-            },
-            error => {
-                var errmess = new Error(error.message);
-                throw errmess;
-            })
-        .then(response => response.json())
-        .then(posts => {
-
-            const logos = posts.map((post, indx) => {
-                return wpGetFeaturedImage(post, null);
-            });
-
-            const featured_logos =  shuffleArray(posts.filter((post, indx) => {
-                for (var i=0 ; i < post.categories.length ; i++) {
-                    if (post.categories[i] === 48) {  /* featured technology */
-                        return true;
-                    }
-                }
-                return false;
-            }).map((featuredPost, indx) => {
-                return wpGetFeaturedImage(featuredPost, null);
-            }));
-
-            /* Begin random logo updates after a 5-second initial delay */
-            var myTimeout = setTimeout(function() {
-                self.repaint();
-            }, 5000);    
-
-            self.setState({
-                logos: logos,
-                featured_logos: featured_logos,
-                repaintDelay: myTimeout
-            });
-
-            }
-        )
-    }
 
     componentDidMount() {
         /* this is a general purpose moratorium on React
@@ -384,6 +335,56 @@ class LogoCube extends Component {
             'assets/images/Redux_Logo-300x80.png',
             'assets/images/wordpress-100x161.png'
         ]);
+    }
+
+    fetcher() {
+        const self = this;
+        fetch(backendUrl + "posts?categories=43&_embed&per_page=100")
+        .then(
+            response => {
+                if (response.ok) {
+                    return response;
+                } else {
+                    var error = new Error('Error ' + response.status + ': ' + response.statusText);
+                    error.response = response;
+                    throw error;
+                }
+            },
+            error => {
+                var errmess = new Error(error.message);
+                throw errmess;
+            })
+        .then(response => response.json())
+        .then(posts => {
+
+            const logos = posts.map((post, indx) => {
+                return wpGetFeaturedImage(post, null);
+            });
+
+            const featured_logos =  shuffleArray(posts.filter((post, indx) => {
+                for (var i=0 ; i < post.categories.length ; i++) {
+                    if (post.categories[i] === 48) {  /* featured technology */
+                        return true;
+                    }
+                }
+                return false;
+            }).map((featuredPost, indx) => {
+                return wpGetFeaturedImage(featuredPost, null);
+            }));
+
+            /* Begin random logo updates after a 5-second initial delay */
+            var myTimeout = setTimeout(function() {
+                self.repaint();
+            }, 5000);    
+
+            self.setState({
+                logos: logos,
+                featured_logos: featured_logos,
+                repaintDelay: myTimeout
+            });
+
+            }
+        )
     }
     
 }
