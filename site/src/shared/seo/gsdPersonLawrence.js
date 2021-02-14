26a4cd03ad740a39846edf93124bad331f410ddd
Lawrence McDaniel <lpm0073@gmail.com>
2021-02-14 10:21:12 -0600

    add meta data for image

--- site/src/shared/seo/gsdPersonLawrence.js
+++ site/src/shared/seo/gsdPersonLawrence.js
@@ -151,7 +151,27 @@ export const gsdPersonLawrenceMcDaniel = {
          }
       ]
    },
-   "image":"https://cdn.lawrencemcdaniel.com/social-1200x675.jpg",
+   "image": {
+      "@type": "ImageObject",
+      "author": "Barry Wolfryd",
+      "creator": "Barry Wolfryd",
+      "contentLocation": "Colonia Roma Norte, CDMX, Mexico",
+      "locationCreated":"Colonia Roma Norte, CDMX, Mexico",
+      "discussionUrl": "https://www.barrywolfryd.com/",
+      "contentUrl": "https://cdn.lawrencemcdaniel.com/social-1200x675.jpg",
+      "datePublished": "2017-06-15",
+      "uploadDate": "2020-06-30",
+      "copyrightYear": "2017",
+      "mainEntityOfPage": {
+         "@type": "WebPage",
+         "@id": "https://lawrencemcdaniel.com/"
+      },
+      "keywords":"Lawrence McDaniel, Barry Wolfryd photography, Roma Norte, CDMX",
+      "description": "Barry Wolfryd did a series of these for an article I wrote in geek.ly",
+      "about": "Barry Wolfryd did a series of these for an article I wrote in geek.ly",
+      "comment": "Barry Wolfryd did a series of these for an article I wrote in geek.ly",
+      "name": "Lawrence McDaniel Portfolio Images"
+    },
    "jobTitle":"Full Stack Developer",
    "sameAs": [
       "https://lawrencemcdaniel.com/",
