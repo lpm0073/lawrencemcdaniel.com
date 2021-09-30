563d7fcd808d09f4f081e16bf00ef78179357bd2
Lawrence McDaniel <lpm0073@gmail.com>
2021-09-30 13:48:22 -0500

    add linkedin qr code

--- site/src/pages/contact/Component.js
+++ site/src/pages/contact/Component.js
@@ -31,38 +31,59 @@ class Contact extends Component {
 
                     <div className="row text-center mt-5">
                         <div className="col-md-1"></div>
-                        <div className="col-md-6 contact-data">
-                            <div>
-                            <a target="_blank" href="https://goo.gl/maps/WJy25bNGit2L8Nzz8" rel="noopener noreferrer">
-                                <i class="fa fa-map-marker fa-3x"></i>
-                                <address className="m-b-4">
-                                Lawrence McDaniel, {baseTitle}<br />
-                                210 Broadway, Suite 200<br />
-                                Cambridge, MA 02139<br />
-                                USA
-                                </address>
-                            </a>
-                            </div>
-                            <h6 className="mb-4">Email: <a href="mailto:lpm0073@gmail.com">lpm0073@gmail.com</a></h6>
-                            <p className="mb-0">Phone: <a className="mt-4 contact-phone-link" href="tel:+16178346172">+1 (617) 834-6172</a></p>
-                            <p>Skype: mcdaniel0073</p>
+                        <div className="col-md-6 contact-data mt-5 pt-5">
+                            <div className="row mt-3"></div>
+                            <div className="">
+                                <a target="_blank" href="https://goo.gl/maps/WJy25bNGit2L8Nzz8" rel="noopener noreferrer">
+                                    <i class="fa fa-map-marker fa-3x"></i>
+                                    <address className="m-b-4">
+                                    Lawrence McDaniel, {baseTitle}<br />
+                                    210 Broadway, Suite 200<br />
+                                    Cambridge, MA 02139<br />
+                                    USA
+                                    </address>
+                                </a>
+                                <h6 className="mb-4">Email: <a href="mailto:lpm0073@gmail.com">lpm0073@gmail.com</a></h6>
+                                <p className="mb-0">Phone: <a className="mt-4 contact-phone-link" href="tel:+16178346172">+1 (617) 834-6172</a></p>
+                                <p>Skype: mcdaniel0073</p>
 
-                            <div className="contact-social-buttons m-4">
-                                <a className="m-1" target="_blank" href="https://linkedin.com/in/lawrencemcdaniel" rel="noopener noreferrer"><i class="fa fa-linkedin-square fa-3x"></i></a>
-                                <a className="m-1" target="_blank" href="https://www.facebook.com/lawrence.p.mcdaniel" rel="noopener noreferrer"><i class="fa fa-facebook-square fa-3x"></i></a>
-                                <a className="m-1" target="_blank" href="https://github.com/lpm0073" rel="noopener noreferrer"><i class="fa fa-github-square fa-3x"></i></a>
+                                <div className="contact-social-buttons m-4">
+                                    <a className="m-1" target="_blank" href="https://linkedin.com/in/lawrencemcdaniel" rel="noopener noreferrer"><i class="fa fa-linkedin-square fa-3x"></i></a>
+                                    <a className="m-1" target="_blank" href="https://www.facebook.com/lawrence.p.mcdaniel" rel="noopener noreferrer"><i class="fa fa-facebook-square fa-3x"></i></a>
+                                    <a className="m-1" target="_blank" href="https://github.com/lpm0073" rel="noopener noreferrer"><i class="fa fa-github-square fa-3x"></i></a>
+                                </div>
+                                <a className="m-4 btn btn-danger" role="button" target="_blank" href={resumeUrl} rel="noopener noreferrer">
+                                    <i className="fa fa-download"></i> Download Resume
+                                </a>
                             </div>
-                            <a className="m-4 btn btn-danger" role="button" target="_blank" href={resumeUrl} rel="noopener noreferrer">
-                                <i className="fa fa-download"></i> Download Resume
-                            </a>
                         </div>
                         <div className="col-md-4 m-4">
-                            <div className="col-md-1"></div>
-                            <div className="col-md-10">
-                                <h4 className="mb-4">Whatsapp QR Code</h4>
-                                <div className="col-md-12 whatsapp-qr-code square"></div>
+                            <div className="row qr-code-container mb-4">
+                                <div className="col-md-1"></div>
+                                <div className="col-md-10">
+                                    <div>
+                                        <h4>
+                                            <span className="col-md-2 mr-2 whatsapp-logo image-container"></span>
+                                            <span className="col-md-10 p-0">Whatsapp QR Code</span>
+                                        </h4>
+                                    </div>
+                                    <div className="col-md-12 whatsapp-qr-code square image-container"></div>
+                                </div>
+                                <div className="col-md-1"></div>
                             </div>
-                            <div className="col-md-1"></div>
+                            <div className="row qr-code-container">
+                                <div className="col-md-1"></div>
+                                <div className="col-md-10">
+                                    <div>
+                                        <h4>
+                                            <span className="col-md-2 mr-2 linkedin-logo image-container"></span>
+                                            <span className="col-md-10 p-0">LinkedIn QR Code</span>
+                                        </h4>
+                                    </div>
+                                    <div className="col-md-12 linkedin-qr-code square image-container"></div>
+                                </div>
+                                <div className="col-md-1"></div>
+                            </div>
                         </div>
                         <div className="col-md-1"></div>
                     </div>
