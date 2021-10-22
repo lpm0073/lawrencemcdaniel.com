2e81cb3c83db2f6f964cab000cdb8cd3305fe6ec
Lawrence McDaniel <lpm0073@gmail.com>
2021-10-22 14:45:38 -0500

    remove trailing slash

--- site/src/shared/urls.js
+++ site/src/shared/urls.js
@@ -1,5 +1,5 @@
 export const baseUrl = 'http://localhost:3001/';
 export const resumeUrl = 'https://cdn.lawrencemcdaniel.com/doc/Resume-Lawrence-McDaniel-202006.pdf';
 export const backendUrl = 'https://api.lawrencemcdaniel.com/wp-json/wp/v2/';
-export const siteUrl = 'https://lawrencemcdaniel.com/';
-export const cdnUrl = 'https://cdn.lawrencemcdaniel.com/';
+export const siteUrl = 'https://lawrencemcdaniel.com';
+export const cdnUrl = 'https://cdn.lawrencemcdaniel.com';
