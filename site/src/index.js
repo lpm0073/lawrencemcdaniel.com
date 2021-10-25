0692d55be181e6ecb050eb7c37d7678ecd258c21
Lawrence McDaniel <lpm0073@gmail.com>
2021-10-25 15:51:44 -0500

    setup workbox and service worker

--- site/src/index.js
+++ site/src/index.js
@@ -1,14 +1,19 @@
 /*
 https://blog.bitsrc.io/lazy-loading-react-components-with-react-lazy-and-suspense-f05c4cfde10c
+
+mcdaniel oct-2021: also see https://blog.bitsrc.io/using-service-workers-with-react-27a4c5e2d1a9
  */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
+
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap-social/bootstrap-social.css';
 import './index.css';
-import * as serviceWorker from './serviceWorker';
-import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -19,5 +24,10 @@ ReactDOM.render(
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
