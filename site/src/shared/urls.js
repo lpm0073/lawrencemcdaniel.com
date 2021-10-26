89f3e488d2077d19280df3bd9690c6ba18df0c78
Lawrence McDaniel <lpm0073@gmail.com>
2021-10-26 15:13:27 -0500

    make urls more DRY

--- site/src/shared/urls.js
+++ site/src/shared/urls.js
@@ -1,14 +1,21 @@
 export const baseUrl = 'http://localhost:3001/';
-export const apiUrl = 'https://api.lawrencemcdaniel.com';
-export const siteUrl = 'https://lawrencemcdaniel.com';
-export const cdnUrl = 'https://cdn.lawrencemcdaniel.com';
+
+export const protocol = "https";
+export const domainName = "lawrencemcdaniel.com";
+
+export const CORSOrigins = '*.' + domainName;
+
+export const siteUrl = protocol + '://'  + domainName;
+export const apiUrl = protocol + '://api.' + domainName;
+export const cdnUrl = protocol + '://cdn.' + domainName;
+
 export const resumeUrl = cdnUrl + '/doc/Resume-Lawrence-McDaniel-202006.pdf';
 export const backendUrl = apiUrl + '/wp-json/wp/v2/';
 
 // Wordpress API Content URL end points
-export const CDNSpecialtiesURL = backendUrl + "posts?categories=43&_embed&per_page=100";
-export const CDNPortfolioURL = backendUrl + "posts?categories=47&_embed&per_page=100";
-export const CDNEducationURL = backendUrl + "posts?categories=44&_embed&per_page=100";
-export const CDNRecommendationsURL = backendUrl + "posts?categories=45&_embed&per_page=100";
-export const CDNProjectsURL = backendUrl + "media?include=2324,2320,2319,2300,2295,2296,2297,2298,2299,2301,2302,2303";
-export const CDNClientsURL = backendUrl + "posts?categories=46&_embed&per_page=100";
+export const APISpecialtiesURL = backendUrl + "posts?categories=43&_embed&per_page=100";
+export const APIPortfolioURL = backendUrl + "posts?categories=47&_embed&per_page=100";
+export const APIEducationURL = backendUrl + "posts?categories=44&_embed&per_page=100";
+export const APIRecommendationsURL = backendUrl + "posts?categories=45&_embed&per_page=100";
+export const APIProjectsURL = backendUrl + "media?include=2324,2320,2319,2300,2295,2296,2297,2298,2299,2301,2302,2303";
+export const APIClientsURL = backendUrl + "posts?categories=46&_embed&per_page=100";
