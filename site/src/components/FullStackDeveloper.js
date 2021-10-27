9387d7e09c0432496f52f65949494094bb574f51
Lawrence McDaniel <lpm0073@gmail.com>
2021-10-27 12:00:11 -0500

    refactor sub-title to its own component

---
+++ site/src/components/FullStackDeveloper.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Fade } from 'react-animation-components';
+import {baseTitle} from '../shared/seo/gsdCommon';
+
+const FullStackDeveloper = (props) => {
+
+    console.log(props.cls);
+    const thisClassName = props.cls;
+
+    return ( 
+      <React.Fragment>
+        <div key={props.idx} className={thisClassName}>
+            <p className="jumbotron-subtitle mb-0">{baseTitle}</p>
+            <p className="lead mt-0 mb-0 hide-medium">
+                <span className="open-edx-consultant-bookends" role="img" aria-label="Close">⇥📙📚 </span>
+                <a href="/openedx"><span className="open-edx-consultant">Open edX® Consultant</span></a>
+                <span className="open-edx-consultant-bookends" role="img" aria-label="Close"> 📚📘⇤</span>
+            </p>
+        </div>
+      </React.Fragment>
+    );
+}
+
+export default FullStackDeveloper;
--- site/src/pages/home/Component.js
+++ site/src/pages/home/Component.js
@@ -5,7 +5,9 @@ import * as Actions from '../../redux/ActionCreators';
 
 import { Fade } from 'react-animation-components';
 import { Offline, Online } from "react-detect-offline";
+
 import LogoCube from '../../components/logoCube/Component';
+import FullStackDeveloper from '../../components/FullStackDeveloper';
 import { gsdGraph } from '../../shared/seo/gsdGraph';
 import { gsdPersonLawrenceMcDaniel } from '../../shared/seo/gsdPersonLawrence';
 import {basePageTitle, baseTitle} from '../../shared/seo/gsdCommon';
@@ -50,22 +52,8 @@ class Home extends Component {
                                     <h1 className="display-4">
                                         <span className="px-3 super-bold pre-wrap">Lawrence McDaniel</span>
                                     </h1>
-                                    {this.props.homePage.isSet ? 
-                                        <p className="jumbotron-subtitle pre-wrap">{baseTitle}</p>
-                                        :
-                                        <Fade in> 
-                                            <div className="">
-                                                <p className="jumbotron-subtitle mb-0">{baseTitle}</p>
-                                                <p className="lead mt-0 mb-0 hide-medium">
-                                                    <Online><span className="open-edx-consultant-bookends" role="img" aria-label="Close">⇥📙📚 </span></Online>
-                                                    <Offline><span className="open-edx-consultant-bookends offline" role="img" aria-label="Close">⇥📙📚 </span></Offline>
-                                                    <a href="/openedx"><span className="open-edx-consultant">Open edX® Consultant</span></a>
-                                                    <Online><span className="open-edx-consultant-bookends" role="img" aria-label="Close"> 📚📘⇤</span></Online>
-                                                    <Offline><span className="open-edx-consultant-bookends offline" role="img" aria-label="Close"> 📚📘⇤</span></Offline>
-                                                </p>
-                                            </div>
-                                        </Fade>
-                                    }
+                                    <Online><FullStackDeveloper idx="1" cls="" /></Online>
+                                    <Offline><FullStackDeveloper idx="2" cls="offline" /></Offline>
                                 </div>
                                 <div className="col-lg-6 pt-5 col-md-12">
                                     <LogoCube />
@@ -76,11 +64,11 @@ class Home extends Component {
                     <div className="row mx-0 react-banner">
                         <div className="col-12">
                             <div className="text-right mr-3 mb-1">
-                                <Offline><div className="offline-alert"><span className="fa fa-stop-circle"></span> Check your internet connection.</div></Offline>
+                                <Offline><div key="1" className="offline-alert"><span className="fa fa-stop-circle"></span> Check your internet connection.</div></Offline>
                             </div>
                             <div className="text-right mr-3 mb-2">
-                                <Online><img className="react-logo" src="/assets/images/react-logo-300x261.png" alt="ReactJS logo" /></Online>
-                                <Offline><img className="react-logo offline" src="/assets/images/react-logo-300x261.png" alt="ReactJS logo" /></Offline>
+                                <Online><img key="1" className="react-logo" src="/assets/images/react-logo-300x261.png" alt="ReactJS logo" /></Online>
+                                <Offline><img key="2" className="react-logo offline" src="/assets/images/react-logo-300x261.png" alt="ReactJS logo" /></Offline>
                                 Progressive Web App Built with ReactJS, Redux and Workbox. <a className="mx-1 learn-more-link" href="/reactjs" target="_self">Learn more</a>
                             </div>
                         </div>
