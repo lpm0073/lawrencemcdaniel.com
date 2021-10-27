15d46b31e5af917bbc517d6cbec3b7f1835111c6
Lawrence McDaniel <lpm0073@gmail.com>
2021-10-27 15:10:42 -0500

    adjust responsive styling

--- site/src/components/footer/Component.js
+++ site/src/components/footer/Component.js
@@ -12,16 +12,16 @@ export default function Footer(props) {
         <React.Fragment>
             <footer key="app-footer">
                 <div className="footer">
-                    <div className="row justify-content-center">
+                    <div className="row justify-content-center hide-small">
                         <div className="col-auto">
                             <div className="text-center">                        
-                                <a className="mx-1" href={resumeUrl} target="_blank" rel="noopener noreferrer">Resume</a><span> | </span>
-                                <a className="mx-1" href="https://www.linkedin.com/in/lawrencemcdaniel/" target="_blank" rel="noopener noreferrer">LinkedIn</a><span> | </span>
-                                <a className="mx-1 hide-small" href="https://github.com/lpm0073" target="_blank" rel="noopener noreferrer">Github</a><span className="hide-small"></span>
+                                <a className="mx-1 " href={resumeUrl} target="_blank" rel="noopener noreferrer">Resume</a><span> | </span>
+                                <a className="mx-1 " href="https://www.linkedin.com/in/lawrencemcdaniel/" target="_blank" rel="noopener noreferrer">LinkedIn</a><span> | </span>
+                                <a className="mx-1 " href="https://github.com/lpm0073" target="_blank" rel="noopener noreferrer">Github</a><span className="hide-small"></span>
                             </div>
                         </div>
                     </div>
-                    <div className="row mx-0 react-banner">
+                    <div className="row mx-0 react-banner hide-small">
                         <div className="col-12">
                             <div className="text-center mr-3 mb-2">
                                 <img key="1" className="react-logo" src="/assets/images/react-logo-300x261.png" alt="ReactJS logo" />
