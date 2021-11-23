8fc6f41ea3c8ec283626c5d5efab67477ab201fb
Lawrence McDaniel <lpm0073@gmail.com>
2021-11-23 14:33:32 -0600

    add babel and update sitemap

--- site/src/components/sitemap/generator.js
+++ site/src/components/sitemap/generator.js
@@ -1,16 +1,14 @@
-import { URL_SITE } from './shared/urls';
-
 require("babel-register")({
     presets: ["es2015", "react"]
   });
-   
+
   const router = require("./routes").default;
   const Sitemap = require("react-router-sitemap").default;
   
   function generateSitemap() {
       return (
         new Sitemap(router)
-            .build({URL_SITE})
+            .build("https://lawrencemcdaniel.com/")
             .save("./public/sitemap.xml")
       );
   }
