c639e00c8c8901bf201e80c944537f728fe00821
lpm0073 <lpm0073@gmail.com>
2022-11-18 14:02:41 -0600

    redesign home page w 3 prof titles

--- site/src/pages/home/Component.js
+++ site/src/pages/home/Component.js
@@ -4,12 +4,14 @@ import { bindActionCreators } from "redux";
 import * as Actions from "../../redux/ActionCreators";
 
 import LogoCube from "../../components/logoCube/Component";
-import FullStackDeveloper from "../../components/FullStackDeveloper";
+import OpenedxConsultant from "./OpenedxConsultant";
+import JobTitle from "./JobTitle";
 import { gsdGraph } from "../../shared/seo/gsdGraph";
 import { gsdPersonLawrenceMcDaniel } from "../../shared/seo/gsdPersonLawrence";
 import { basePageTitle } from "../../shared/seo/gsdCommon";
 import { Helmet } from "react-helmet";
 import { URL_SITE } from "../../shared/constants";
+import { baseTitle } from "../../shared/seo/gsdCommon";
 
 import "./styles.css";
 
@@ -58,23 +60,22 @@ class Home extends Component {
           <div className="h-100">
             <div className="row pt-5 m-0 mr-0 px-0 jumbotron-spacer">
               <div className="col-lg-6 pt-5 col-md-12 text-center noselect ">
+                <div className="title-spacer"></div>
                 <h1 className="display-4">
                   <span className="px-3 super-bold pre-wrap">
                     Lawrence McDaniel
                   </span>
                 </h1>
-                <FullStackDeveloper idx="1" cls="" />
+                
               </div>
               <div className="col-lg-6 pt-5 col-md-12">
                 <LogoCube />
               </div>
             </div>
             <div className="row mt-5 text-center hide-medium">
-              <div className="col-12 mt-5">
-                <a className="data-science-link" href="/data-science">
-                  <h2>Data Scientist</h2>
-                </a>
-              </div>
+              <JobTitle idx="1" href="/skills" title={baseTitle} />
+              <OpenedxConsultant />
+              <JobTitle idx="3" href="/data-science" title="Data Scientist" />
             </div>
           </div>
         </div>
