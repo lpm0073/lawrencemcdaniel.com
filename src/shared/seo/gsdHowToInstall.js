9f8ff8b3587188ae97978b7e6bc59f0bc511d0b5
lpm0073 <lpm0073@gmail.com>
2025-08-01 09:11:33 -0500

    chore: fix up HowToDirection

--- src/shared/seo/gsdHowToInstall.js
+++ src/shared/seo/gsdHowToInstall.js
@@ -55,42 +55,52 @@ export const gsdHowToInstallOpenEdx = {
       itemListElement: [
         {
           '@type': 'HowToDirection',
+          name: 'Choose an AWS AMI',
           text: 'Step 1: Choose AMI. AWS provides you with a pick list of several common server operating systems. Technically speaking, each of these options is an AMI (Amazon Machine Image) that AWS internally maintains and makes available to their customers. For you, this means that you’re able to spin up an Ubuntu server in a few seconds, without needing to worry about where the operating system repo might be located and so on.',
         },
         {
           '@type': 'HowToDirection',
+          name: 'Server Sizing',
           text: 'Step 2: Server Sizing. AWS EC2 Server Sizing: After prolonged experimentation I have gravitated to AWS EC2 T2.Large servers as my virtual server configuration of choice. Generalizing, this provides 2 cpu’s and 8gb of memory, which is sufficient resource for a small production implementation. As of September 2018 AWS charges $0.0928 per Hour ($67 USD per month) for a t2.large server. You can dramatically reduce the cost of the server by purchasing a one-year contract for the server instance, in which case the cost reduces to $42 USD per month. I launch these with 100gb of drive space, which thus far has been far more than sufficient for my needs. Bear in mind that my recommendation is almost exactly double that provided in the official edX documentation. My view on this is that it’s already challenging enough to get this platform up and running without adding unnecessary challenge by under-sizing your equipment. Very generally speaking, this server config should handle a couple hundred concurrent learners. Look at the bottom of this page for links to downstream repos that are part of a simple horizontal scaling strategy for small (but not tiny) institutions.',
         },
         {
           '@type': 'HowToDirection',
+          name: 'Instance Details',
           text: 'Step 3: Instance Details. The default values provided in this screen are what you want. Later on it would be a great idea to revisit these settings to get a better understanding of your infrastructure-level configuration options.',
         },
         {
           '@type': 'HowToDirection',
+          name: 'Add Storage',
           text: 'Step 4: Add Storage. AWS instances by default come with 8gb of “hard drive” storage. However, you can modify this. You’ll need at least 50gb of storage for Open edX plus normal amounts of data. I suggest doubling that amount, to 100gb. I have not seen that this impacts the cost of the server in any meaningful way.',
         },
         {
           '@type': 'HowToDirection',
+          name: 'Add Tags',
           text: 'Step 5: Add Tags. Tags are a way to identify AWS resources inside your account. This is only important if you have many resources (for example, many server instances) in existence in your AWS account. Otherwise you can skip this step.',
         },
         {
           '@type': 'HowToDirection',
+          name: 'Security Profile',
           text: 'Step 6: Security Profile. This is important. You manage server port settings separately from the server itself. Generalizing, you create a port security profile, and then assign this profile to your EC2 instance. Open edX uses many ports as part of the standard installation. Note that this script installs ALL modules, and you therefore need to open many ports. Open edX default http addressing uses port numbers rather than subdomains or url paths/routes. The ports in the screen shot that follows correspond with the following Open edX modules.',
         },
         {
           '@type': 'HowToDirection',
+          name: 'Review Instance Prior to Launch',
           text: 'Step 7: Review Instance Prior to Launch. ',
         },
         {
           '@type': 'HowToDirection',
+          name: 'Setup SSH Key Pair',
           text: 'Step 8: Setup an SSH key pair. You’ll use a terminal emulator via SSH to connect to your server. If you’re unfamiliar with how to connect to a linux server then you can start your journey here: http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/putty.html. Note that you will run this script as well as execute nearly all Open edX admin utilities from a linux command line. So, if you’re new to this then you should bite the bullet and do some online self-study on using linux terminal emulators and SSH.',
         },
         {
           '@type': 'HowToDirection',
+          name: 'Launch Status',
           text: 'Step 9: Launch Status. Click the blue “View Instances” button at the bottom-right of this screen.',
         },
         {
           '@type': 'HowToDirection',
+          name: 'EC2 Instance Console',
           text: 'Step 10: EC2 Instance Console. If this is your very first EC2 instance then you’ll see a single row on this screen that shows the vital signs of your new Linux virtual server. It will take a few minutes for the server to instantiate itself and come online. When the server is ready the “Instance State” field will read “Running” and the icon color will change from yellow to green.',
         },
       ],
@@ -106,10 +116,12 @@ export const gsdHowToInstallOpenEdx = {
       itemListElement: [
         {
           '@type': 'HowToTip',
+          name: 'Monitor Script Progress',
           text: 'I suggest using up to three additional terminal windows to monitor progress of the script.<ul><li>the linux top command is similar to Windows and OSX’s “System Activities” or “System Monitor” windows. The server cpu usage will remain steady at around 50% cumulative usage while the script is running.</li><li>the command sudo /edx/bin/supervisorctl status will print the Open edX processes that are currently running. You’ll see this list grow as the script progresses</li><li>using ls to explore the children folders of /edx/ will at a minimum be informative.</li></ul>',
         },
         {
           '@type': 'HowToDirection',
+          name: 'Run the Script',
           text: 'This script takes around ONE HOUR to run and is intended to be spawned on a background process as follows:',
         },
       ],
@@ -125,14 +137,17 @@ export const gsdHowToInstallOpenEdx = {
       itemListElement: [
         {
           '@type': 'HowToDirection',
+          name: 'Verify Script Completion',
           text: 'Once the script is complete you should be able to open the landing pages for both the LMS and the CMS. See below for additional information about the many port assignments and URL oddities of Open edX. More immediately, the landing pages for the LMS and CMS should look like the following:',
         },
         {
           '@type': 'HowToDirection',
+          name: 'Check Open edX Modules',
           text: 'Using the following admin command line script, you can view which Open edX modules are currently running: sudo /edx/bin/supervisorctl status',
         },
         {
           '@type': 'HowToDirection',
+          name: 'Verify Installation Success',
           text: 'If the installation was successful then you should see the following:',
         },
       ],
@@ -158,6 +173,7 @@ export const gsdHowToInstallOpenEdx = {
       itemListElement: [
         {
           '@type': 'HowToDirection',
+          name: 'Create a Superuser',
           text: 'You can save yourself a lot of future busy work by creating a root / admin account in this instance, prior to creating your AMI. Following are the commands to create a new superuser from the terminal command line:',
         },
       ],
@@ -175,10 +191,12 @@ export const gsdHowToInstallOpenEdx = {
       itemListElement: [
         {
           '@type': 'HowToDirection',
+          name: 'Create an AMI',
           text: 'Read more here to learn about what an AMI (Amazon Machine Image) is, and how it is used: http://docs.aws.amazon.com/elasticbeanstalk/latest/dg/using-features.customenv.html. Meanwhile, here’s a screen shot short cut that at least points you in the right direction of where to go and what to do.',
         },
         {
           '@type': 'HowToDirection',
+          name: 'AMI Creation',
           text: 'It will take around 15 minutes for the AWS Image to render. Once the process has completed, and only after the process has completed, you can terminate the original EC2 instance from the EC2 instance console.',
         },
       ],
@@ -194,10 +212,12 @@ export const gsdHowToInstallOpenEdx = {
       itemListElement: [
         {
           '@type': 'HowToDirection',
+          name: 'Configuration Files',
           text: 'The native installation program creates four json files located in /edx/app/edxapp/ which contain the most important Django application parameters for your Open edX platform including for example, the name of your platform and the domain name. Unfortunately, the files are not user friendly, and to make matters worse, there is no way to add documentation to a JSON file.',
         },
         {
           '@type': 'HowToDirection',
+          name: 'Configuration Guidelines',
           text: 'Some basic configuration guidelines<ul><li>There are only four configuration files for the entire platform. These are located in /edx/app/edxapp</li><li>Avoid editing lms.auth.json and cms.auth.json. These two files contain the dozens of passwords that the Open edX platform created during the native installation procedure. Other than setting the password for your SMTP email account, there is virtually no reason for you to edit either of these files. A copy of the original password values exists in the file ~/my-passwords.yml.</li><li>Any changes to any of these four jason files take effect after restarting the platform. The following command line instructions will restart your platform.</li></ul>',
         },
       ],
