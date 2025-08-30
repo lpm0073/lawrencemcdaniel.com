220329a08be12aa9257bb53f3409ed125429cde4
lpm0073 <lpm0073@gmail.com>
2025-08-29 18:13:41 -0600

    chore: tweak image prefetching refactor

--- src/shared/wpGetFeaturedImage.js
+++ src/shared/wpGetFeaturedImage.js
@@ -1,8 +1,7 @@
-
-
 export const wpGetFeaturedImage = (post, imageSize = 'medium') => {
   let image_url =
-    post?._embedded?.['wp:featuredmedia']?.[0]?.media_details?.sizes?.[imageSize]?.source_url ||
+    post?._embedded?.['wp:featuredmedia']?.[0]?.media_details?.sizes?.[imageSize]
+      ?.source_url ||
     post?._embedded?.['wp:featuredmedia']?.[0]?.source_url ||
     post?.source_url ||
     null
